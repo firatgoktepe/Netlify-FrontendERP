56a777f3747fa439504061cf89aad1ee5d135a6e
DeveloperFG <firatgoktepe1986@gmail.com>
2022-11-30 16:46:13 +0300

    Route for Dashboard created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddProject from './components/AddProject'
 import Home from './components/Home'
+import Dashboard from './components/Dashboard';
 import AddProjectsStatusReport from './components/AddProjectsStatusReport';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="addproject" element={<AddProject />} />
         <Route path="add-projects-status-report" element={<AddProjectsStatusReport />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );  
