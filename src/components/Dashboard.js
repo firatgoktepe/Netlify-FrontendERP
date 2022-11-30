29fdd767e57f16e117c5782e6c38de5665dfb513
DeveloperFG <firatgoktepe1986@gmail.com>
2022-11-30 16:45:26 +0300

    Dashboard structure added

---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+const Dashboard = () => {
+  return (
+    <>
+        <Header />
+        <div className="container">
+            <div className="row">
+                <div>Dashboard</div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Dashboard
