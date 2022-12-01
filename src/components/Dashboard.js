a356db22f493a53525f9278c2466abfa5f0a9934
DeveloperFG <firatgoktepe1986@gmail.com>
2022-12-01 16:45:46 +0300

    first tremor ui component added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import Header from './Header'
+import {
+  Card,
+  Title,
+  Text,
+  Tab,
+  TabList,
+  ColGrid,
+  Block,
+  Flex,
+  Metric,
+  BadgeDelta,
+  ProgressBar,
+} from '@tremor/react';
 
 const Dashboard = () => {
   return (
@@ -8,6 +21,20 @@ const Dashboard = () => {
         <div className="container">
             <div className="row">
                 <div>Dashboard</div>
+                <Card maxWidth="max-w-lg">
+                  <Flex alignItems="items-start">
+                    <Block>
+                      <Text>Sales</Text>
+                      <Metric>$ 20,699</Metric>
+                    </Block>
+                    <BadgeDelta deltaType="moderateDecrease" text="13.2%" />
+                  </Flex>
+                  <Flex marginTop="mt-4">
+                    <Text truncate={true}>50% ($ 110,250)</Text>
+                    <Text> $ 220,500 </Text>
+                  </Flex>
+                  <ProgressBar percentageValue={50} marginTop="mt-2" />
+                </Card>
             </div>
         </div>
     </>
